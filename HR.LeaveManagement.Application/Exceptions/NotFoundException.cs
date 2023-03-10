5ce0977d0029ff981e31852226558b28f91771cb
luan AC <luanalbucan@hotmail.com>
2023-03-10 16:38:02 -0300

    continue adding

---
+++ HR.LeaveManagement.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string? message, object? key) : base(message, key)
+        {
+        }
+    }
+}
