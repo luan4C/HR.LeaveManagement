de8cbaf8524334488a7b2496811f6ea10aca0a82
aegishjalmur15 <luan.albuquerque.candeia12@gmail.com>
2023-03-24 23:50:11 -0300

    feat: add update leave request feature

--- HR.LeaveManagement.Application/Features/LeaveRequest/Commands/UpdateLeaveRequest/UpdateLeaveRequestCommandHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveRequest/Commands/UpdateLeaveRequest/UpdateLeaveRequestCommandHandler.cs
@@ -1,5 +1,9 @@
-﻿using HR.LeaveManagement.Application.Contracts.Logging;
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Logging;
 using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
+using HR.LeaveManagement.Application.Features.LeaveAllocation.Commands.CreateLeaveAllocation;
+using HR.LeaveManagement.Application.Features.LeaveRequest.Shared;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -13,16 +17,35 @@ namespace HR.LeaveManagement.Application.Features.LeaveRequest.Commands.UpdateLe
     {
         private readonly ILeaveRequestRepository _leaveRequestRepository;
         private readonly IAppLogger<UpdateLeaveRequestCommandHandler> _logger;
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IMapper _mapper;
 
-        public UpdateLeaveRequestCommandHandler(ILeaveRequestRepository leaveRequestRepository, IAppLogger<UpdateLeaveRequestCommandHandler> logger)
+        public UpdateLeaveRequestCommandHandler(ILeaveRequestRepository leaveRequestRepository, IAppLogger<UpdateLeaveRequestCommandHandler> logger, ILeaveTypeRepository leaveTypeRepository, IMapper mapper)
         {
             _leaveRequestRepository = leaveRequestRepository;
             _logger = logger;
+            _leaveTypeRepository = leaveTypeRepository;
+            _mapper = mapper;
         }
 
         public async Task<Unit> Handle(UpdateLeaveRequestCommandRequest request, CancellationToken cancellationToken)
         {
-            var leaveRequest = await _leaveRequestRepository.GetByIdAsync(request.Id); 
+            var validator = new UpdateLeaveRequestCommandValidator(_leaveRequestRepository,_leaveTypeRepository);
+
+            var validatorResult = await validator.ValidateAsync(request);
+
+            if (validatorResult.Errors.Any())
+            {
+                _logger.LogWarning($"{nameof(UpdateLeaveRequestCommandRequest)} is Invalid");
+                throw new BadRequestException("Invalid Leave Allocation", validatorResult);
+            }
+            var leaveRequest = await _leaveRequestRepository.GetByIdAsync(request.Id);
+
+            _mapper.Map(request, leaveRequest);
+
+            await _leaveRequestRepository.UpdateAsync(leaveRequest);
+
+            return Unit.Value;
         }
     }
 }
---
+++ HR.LeaveManagement.Application/Features/LeaveRequest/Shared/BaseLeaveRequestValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequest.Shared
+{
+    public class BaseLeaveRequestValidator: AbstractValidator<BaseLeaveRequest>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        public BaseLeaveRequestValidator(ILeaveTypeRepository leaveTypeRepository)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+            RuleFor(src=>src.EndDate).GreaterThan(src=>src.StartDate)
+                .WithMessage("{PropertyName} must be after {ComparisonValue}");
+            RuleFor(src => src.StartDate).GreaterThan(src => src.EndDate)
+                .WithMessage("{PropertyName} must be before {ComparisonValue}");
+
+            RuleFor(src => src.LeaveTypeId).GreaterThan(0)
+                .MustAsync(LeaveTypeMustExist)
+                .WithMessage("{PropertyName} does not exist");
+        }
+
+        private async Task<bool> LeaveTypeMustExist(int leaveTypeId, CancellationToken arg2)
+        {
+            var leaveType= await _leaveTypeRepository.GetByIdAsync(leaveTypeId);
+            return leaveType!= null;
+        }
+    }
+}
