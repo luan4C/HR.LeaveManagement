0cabc418a72c49b8b72978f7c2a64cff2ca8386d
aegishjalmur15 <luan.albuquerque.candeia12@gmail.com>
2023-03-26 22:37:29 -0300

    Adding httpClient

---
+++ HR.LeaveManagemente.BlazorUI/Services/Base/BaseHttpService.cs
@@ -0,0 +1,11 @@
+﻿namespace HR.LeaveManagemente.BlazorUI.Services.Base
+{
+    public partial class BaseHttpService
+    {
+        protected IClient _client;
+         public BaseHttpService(IClient client)
+        {
+            _client = client;
+        }
+    }
+}
