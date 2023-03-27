88b1da5e3f7c5150d96e2f6080f05fed395124b1
aegishjalmur15 <luan.albuquerque.candeia12@gmail.com>
2023-03-26 22:43:56 -0300

    feat: add HttpClient injection

--- HR.LeaveManagemente.BlazorUI/Program.cs
+++ HR.LeaveManagemente.BlazorUI/Program.cs
@@ -1,3 +1,4 @@
+using HR.LeaveManagement.BlazorUI.Services.Base;
 using HR.LeaveManagemente.BlazorUI;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -6,6 +7,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddHttpClient<IClient, Client>(client => 
+client.BaseAddress = new Uri("https://localhost:7246"));
 
 await builder.Build().RunAsync();
