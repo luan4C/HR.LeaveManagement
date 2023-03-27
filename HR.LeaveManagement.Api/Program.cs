10cafdaba83eddc961de678d6fe71bc774a24e3e
luan AC <luanalbucan@hotmail.com>
2023-03-27 16:07:10 -0300

    feat: added razor page to list leave types

--- HR.LeaveManagement.Api/Program.cs
+++ HR.LeaveManagement.Api/Program.cs
@@ -9,7 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddPersistenceServices(builder.Configuration);
-
+builder.Services.AddCors(ops=> {
+    ops.AddPolicy("all", bdr => bdr.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddCors(options =>
@@ -27,7 +29,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors("all");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
